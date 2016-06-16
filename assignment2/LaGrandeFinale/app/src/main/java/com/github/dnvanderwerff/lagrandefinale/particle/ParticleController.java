bc2b889ee8095d9954003eb106670eb40ac17316
dnvanderwerff <daniellevanderwerff@gmail.com>
2016-06-16 23:25:26 +0200

    Started with implementation of recovery, not functioning correctly yet

--- assignment2/LaGrandeFinale/app/src/main/java/com/github/dnvanderwerff/lagrandefinale/particle/ParticleController.java
+++ assignment2/LaGrandeFinale/app/src/main/java/com/github/dnvanderwerff/lagrandefinale/particle/ParticleController.java
@@ -1,5 +1,6 @@
 package com.github.dnvanderwerff.lagrandefinale.particle;
 
+import android.provider.Telephony;
 import android.util.Log;
 
 import com.github.dnvanderwerff.lagrandefinale.MainActivity;
@@ -9,19 +10,26 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 /**
  * Created by Jeroen on 21/05/2016.
  */
 public class ParticleController {
-    private CollisionMap map;
-    private Particle[] particles;
-    private NormalDistribution ndDirection, ndStepSize;
-    private List<Particle> alives, deads;
-    private Random r;
-    private double surface, totalSurface;
-    private int[] cellDist;
-    private String activeCell;
+    private CollisionMap map;                               // The collision map
+    private Particle[] particles;                           // Array of all particles within the map
+    private NormalDistribution ndDirection, ndStepSize;     // Distribution from which to obtain direction and step sizes
+    private List<Particle> alives, deads;                   // List of dead and living particles
+    private Random r;                                       // Random variable
+    private double surface, totalSurface;                   // Surface of location where the user is most likely to be, and total surface
+    private int[] cellDist;                                 // Array indicating how the particles are distributed among the cells
+    private String activeCell;                              // Text displaying which cells are dominantly occupied by particles
+
+    private List<Cluster> previousClusters, currentClusters; // List of cluster for previous and current iteration of move()
+    private LinkedList<Cluster> deadClusters;                // Sorted list of most recently deceased clusters
+    private NormalDistribution ndCluster;                    // Normal distribution for placing particles in recovered cluster
+    private static int MAX_RECOVERED_CLUSTERS = 3;           // Max nr of clusters that will be revived when all particles are gone
+    private static int MAX_DEAD_CLUSTERS = 5;                // Max nr of dead clusters program keeps track of
 
     public double getSurface() { return surface; }
     public double getSurfaceFraction() { return surface / totalSurface; }
@@ -29,12 +37,16 @@ public class ParticleController {
     public ParticleController(CollisionMap map) {
         this.map = map;
         ndDirection = new NormalDistribution(0, Math.toRadians(13));
-        //ndStepSize = new NormalDistribution(0.7, 0.15);
-        ndStepSize = new NormalDistribution(MainActivity.length*0.41, 0.05);
+        ndStepSize = new NormalDistribution(MainActivity.length*0.41, 0.15);    // in meters
         alives = new ArrayList<>();
         deads = new LinkedList<>();
         r = new Random(System.nanoTime());
         cellDist = new int[map.getCellCount() ];
+
+        previousClusters = new ArrayList<Cluster>();
+        currentClusters = new ArrayList<Cluster>();
+        deadClusters = new LinkedList<Cluster>();
+        ndCluster = new NormalDistribution(0,0.5);  // TODO test of deze sd wel een handige waarde is
 
         surface = 0;
 
@@ -112,6 +124,8 @@ public class ParticleController {
             }
         }
         //long moved = System.currentTimeMillis();
+
+
         // Reposition dead particles
         // But not if there are none alive
         if (alives.size() > 0) {
@@ -120,12 +134,49 @@ public class ParticleController {
                 particle.x = dest.x;
                 particle.y = dest.y;
             }
-        } else {
+        }
+
+        // Find current clusters
+        currentClusters = findCluster(alives);
+
+        // Logging
+        String current = "";
+        String previous = "";
+        for (int i = 0; i < currentClusters.size(); i++) {
+            current += map.getCell(currentClusters.get(i).x,currentClusters.get(i).y);
+        }
+        for (int i = 0; i < previousClusters.size(); i++) {
+            previous += map.getCell(previousClusters.get(i).x, previousClusters.get(i).y);
+        }
+        Log.d("previous", "Previous clusters: " + previous);
+        Log.d("current","Current clusters: " + current);
+
+        // Compare with previous clusters to find deceased clusters
+        for (Cluster prev : previousClusters) {
+
+            if (! currentClusters.contains(prev)) {
+                deadClusters.addFirst(prev);
+                if (deadClusters.size() > MAX_DEAD_CLUSTERS) {
+                    deadClusters.removeLast();
+                }
+            }
+        }
+        Log.d("Dead", "Dead clusters: " + String.format(deadClusters.toString()));
+
+        previousClusters = currentClusters;
+
+        if (alives.size() == 0) {
             // At this point, the system needs to recover, as the cluster of particles is in the wrong location.. what is this called again?
 
+            int toRecover = Math.min(deadClusters.size(), MAX_RECOVERED_CLUSTERS);
+            int n = particles.length / toRecover;
+
+            for (int i = 0; i < toRecover; i++) {
+                placeParticles(deadClusters.get(i), n);
+            }
+
         }
-        //long resampled = System.currentTimeMillis();
-        //Log.d("ParticleTime", begin + "-" + moved + "-" + resampled);
+
         // Calculate particle area
         surface = (maxX - minX) * (maxY - minY);
 
@@ -141,6 +192,7 @@ public class ParticleController {
         }
         if (activeCell.equals(""))
             activeCell = "No dominant cells";
+
     }
 
     public Particle[] getParticles() {
@@ -150,4 +202,56 @@ public class ParticleController {
     public String getActiveCell() {
         return activeCell;
     }
+
+
+    // TODO IMPLEMENT THIS FUNCTION CORRECTLY.
+    //
+    // TODO FIRST, TRY SOMETHING EASY: RIGHT NOW IT JUST ASSUMES CENTRE OF THE CELL IN WHICH THE CLUSTER IS LOCATED.
+    // TODO (this will not work if cluster is right in the middle of two cells for example.)
+    //
+    // TODO I don't really understand the x and y values that are returned from the cells in the map.getCells array.. Not what I expect them to be.
+    // TODO so currently recovery does not result in a new cluster at the correct location
+    public List<Cluster> findCluster(List<Particle> particles) {
+        // Use cellDistr to get an estimation of where clusters are, then compute actual centres.
+
+        List<Cluster> foundClusters = new ArrayList<Cluster>();
+
+        double threshold = particles.size() / 3.0;    // TODO check of dit goede waarde is
+        double x, y;
+
+        // TODO getCells has length of 18, while there are 21 cells.. Solve this by modifying MapView and CollisionMap.
+        for (int i = 0; i < map.getCells().length; i++) {
+            if (cellDist[i] >= threshold) {
+                // There is probably a cluster in the cell. Store center of cell as estimation of cluster centre.
+                x = (map.getCells())[i].x;
+                y = (map.getCells())[i].y;
+                foundClusters.add(new Cluster(x,y));
+            }
+        }
+
+        return foundClusters;
+    }
+
+    /* Place n particles around cluster centre */
+    public void placeParticles(Cluster cluster, int n) {
+
+        // Place n particles on the map around provided cluster centre
+        for (int i = 0; i < n && deads.size() > 0; i++) {
+            Particle p = deads.get(0);
+
+            do {
+                p.x = cluster.x + ndCluster.nextValue();
+                p.y = cluster.y + ndCluster.nextValue();
+            } while (map.getCell(p.x,p.y) != 0);
+
+            cellDist[map.getCell(p.x,p.y)] += 1;
+
+            deads.remove(0);
+        }
+
+        // TODO update minX and minY to calculate surface
+        // TODO indicate what the new dominating cells are
+
+    }
+
 }
