166b088527de3fd7a468afada8d336ab68a129c9
dnvanderwerff <daniellevanderwerff@gmail.com>
2016-06-20 19:10:27 +0200

    Updates van recovery deel

--- assignment2/LaGrandeFinale/app/src/main/java/com/github/dnvanderwerff/lagrandefinale/particle/Cluster.java
+++ assignment2/LaGrandeFinale/app/src/main/java/com/github/dnvanderwerff/lagrandefinale/particle/Cluster.java
@@ -5,12 +5,14 @@ package com.github.dnvanderwerff.lagrandefinale.particle;
  */
 public class Cluster {
 
-    public double x, y; // x and y coordinates of cluster centre in meters
+    public double x, y;         // x and y coordinates of cluster centre in meters
+    public int nrParticles;     // Nr of particles in the cluster
 
 
     public Cluster(double xCoord, double yCoord) {
         this.x = xCoord;
         this.y = yCoord;
+        this.nrParticles = 1;
 
     }
 
